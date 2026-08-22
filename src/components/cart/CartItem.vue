b7cf4227bd4c8f1d962eb29bb277839e56f793b6
Ajay.Prasath <ajay.prasath@BLR-MAC-104.local>
2026-08-22 23:12:06 +0530

    feature:add cart page

---
+++ src/components/cart/CartItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="cart-item">
+
+<img :src="item.productImage" :alt="item.productTitle">
+
+<div>
+    <h3>{{ item.productTitle}}</h3>
+    <p>Seller : {{ item.sellerName }}</p>
+    <p>Price : {{ item.price.toLocaleString("en-IN")}}</p>
+    <p>Quantity : {{ item.quantity  }}</p>
+    <p>  Total: ₹{{ item.totalPrice.toLocaleString("en-IN") }}</p>
+</div>
+
+  </div>
+</template>
+
+<script setup>
+defineProps({
+    item:{
+   type:Object,
+   required:true
+    }
+})
+</script>
+<style>
+
+</style>
